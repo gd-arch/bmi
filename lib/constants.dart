609a0942bfa0f92e42d4ba654a5740ecc9641ef3
abc <abc@example.com>
2020-12-04 19:56:06 +0530

    initial

---
+++ lib/constants.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+const double kBottomButtonHeight=70;
+const  KactiveCardColor=Color(0xFF1D1E33);
+const KinacticeCardColor=Color(0xFF111328);
+const KbottomCardColor=Color(0xFFEB1555);
+const KlabelTextStyle=TextStyle(
+ fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+ const KTextNumberStyle=   TextStyle(
+fontSize:50.0,
+fontWeight:FontWeight.w900
+);
+BoxConstraints kSizeConstraints = BoxConstraints.tightFor(
+ width: 56.0,
+ height: 56.0,
+);
+const KTextStyleBottomButtom=TextStyle(
+
+ fontSize: 30,
+
+
+);
+const KTitleTextStyle=TextStyle(
+ fontWeight: FontWeight.bold,
+ fontSize: 50,
+);
+const KresultTextStyle=TextStyle(
+color: Color(0xFF24D876) ,
+ fontSize: 22,
+ fontWeight: FontWeight.bold
+);
+const KBMITextStyle=TextStyle(
+fontWeight: FontWeight.bold,
+ fontSize: 100
+);
